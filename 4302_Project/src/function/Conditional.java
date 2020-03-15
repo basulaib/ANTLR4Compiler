d65502f009c2c0e700dda350803feefe9ac05a3d
Leo <shiyi.du@qq.com>
2020-03-15 19:04:56 -0400

    Update Conditional.java

--- 4302_Project/src/function/Conditional.java
+++ 4302_Project/src/function/Conditional.java
@@ -1,25 +1,41 @@
 package function;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import expression.*;
 import visitor.Visitor;
 
 public class Conditional extends FuncStatement {
-	List<FuncStatement> conditionalStatements;
-	
+	HashMap<Expression, List<FuncStatement>> exprBlocks;
+	List<Expression> order;
+//	List<FuncStatement>conditionalStatements;
+
 	public Conditional() {
-		this.conditionalStatements = new ArrayList<FuncStatement>();
+		this.exprBlocks = new HashMap<Expression, List<FuncStatement>>();
+		this.order = new ArrayList<Expression>();
 	}
-	
-	public Conditional(List<FuncStatement> conditionalStatements) {
-		this.conditionalStatements = conditionalStatements;
+
+	public Conditional(HashMap<Expression, List<FuncStatement>> exprBlocks) {
+		this.exprBlocks = exprBlocks;
 	}
-	
-	public void addConditionalStatement(FuncStatement statement) {
-		this.conditionalStatements.add(statement);
+
+	public void addConditionalStatement(Expression condition, FuncStatement statement) {
+		if (this.exprBlocks.containsKey(condition)) {
+			exprBlocks.get(condition).add(statement);
+		} else {
+			List<FuncStatement> statements = new ArrayList<FuncStatement>();
+			statements.add(statement);
+			exprBlocks.put(condition, statements);
+			this.order.add(condition);
+		}
 	}
-	
+
+	public List<Expression> getOrder() {
+		return this.order;
+	}
+
 	@Override
 	public void accept(Visitor visitor) {
 		visitor.visitConditional(this);
