c3b2686c8ade440955fb444e3bced1d3debae0b5
arules15 <arules14@gmail.com>
2020-03-19 21:48:23 -0400

    added parameter functionality

--- 4302_Project/src/function/Function.java
+++ 4302_Project/src/function/Function.java
@@ -7,40 +7,47 @@ import expression.Expression;
 import visitor.Visitor;
 
 public class Function {
+	private List<Parameter> parameterList;
 	private List<FuncStatement> statementList;
 	private PreCondition preCondition;
 	private PostCondition postCondition;
 	
 	public Function() {
+		this.parameterList = new ArrayList<Parameter>();
 		this.statementList = new ArrayList<FuncStatement>();
 		this.preCondition = new PreCondition();
 		this.postCondition = new PostCondition();
 	}
 	
 	public Function(Function function) {
+		this.parameterList = new ArrayList<Parameter>();
 		this.statementList = function.getStatements();
 		this.preCondition = new PreCondition();
 		this.postCondition = new PostCondition();
 	}
 	
 	public Function(Function function, PreCondition preCondition) {
+		this.parameterList = new ArrayList<Parameter>();
 		this.statementList = function.getStatements();
 		this.preCondition = new PreCondition(preCondition);
 		this.postCondition = new PostCondition();
 	}
 	
 	public Function(Function function, PostCondition postCondition) {
+		this.parameterList = new ArrayList<Parameter>();
 		this.statementList = function.getStatements();
 		this.preCondition = new PreCondition();
 		this.postCondition = new PostCondition(postCondition);
 	}
 	
 	public Function(PreCondition preCondition, PostCondition postCondition) {
+		this.parameterList = new ArrayList<Parameter>();
 		this.statementList = new ArrayList<FuncStatement>();
 		this.preCondition = new PreCondition(preCondition);
 		this.postCondition = new PostCondition(postCondition);
 	}
 	public Function(Function function, PreCondition preCondition, PostCondition postCondition) {
+		this.parameterList = new ArrayList<Parameter>();
 		this.statementList = function.getStatements();
 		this.preCondition = new PreCondition(preCondition);
 		this.postCondition = new PostCondition(postCondition);
@@ -58,6 +65,10 @@ public class Function {
 	public void addPostCondition(Expression postCond) {
 		this.postCondition.addPostCondition(postCond);
 	}
+
+	public void addParameter(Parameter param) {
+		this.parameterList.add(param);
+	}
 	
 	public List<Expression> getPreCondition(){
 		return this.preCondition.getPreConditions();
@@ -70,6 +81,9 @@ public class Function {
 	public List<FuncStatement> getStatements(){
 		return this.statementList;
 	}
+
+	public List<Paramter> getParameters() {return this.parameterList; }
+
 	public void accept(Visitor visitor) {
 		visitor.visitFunction(this);
 	}
---
+++ 4302_Project/src/function/Parameter.java
@@ -0,0 +1,32 @@
+package function;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import expression.Expression;
+import visitor.Visitor;
+
+public class Parameter {
+
+    private String Type;
+    private String ID;
+    private Constant value;
+
+    public Parameter(String type, String ID, Constant value){
+        this.Type = type;
+        this.ID = ID;
+        this.value = value;
+    }
+
+    public String getType(){
+        return this.Type;
+    }
+
+    public String getID() {
+        return this.ID;
+    }
+
+    public String getValue{
+        return this.value;
+    }
+}
