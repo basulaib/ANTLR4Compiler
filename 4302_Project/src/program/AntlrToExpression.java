18018d93ef1ab0ea5687f496f85e7a9b6a713612
arules15 <arules14@gmail.com>
2020-03-22 19:37:53 -0400

    tentatively finihed antlrtoexpression

--- 4302_Project/src/program/AntlrToExpression.java
+++ 4302_Project/src/program/AntlrToExpression.java
@@ -1,11 +1,13 @@
-//package program;
-//
-//import java.util.HashMap;
-//import java.util.HashSet;
-//import java.util.List;
-//
-//import org.antlr.v4.runtime.Token;
-//
+package program;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import org.antlr.v4.runtime.Token;
+
 //import antlr.ExprBaseVisitor;
 //import antlr.ExprParser.BooleanContext;
 //import antlr.ExprParser.ConjunctionContext;
@@ -18,9 +20,114 @@
 //import antlr.ExprParser.NegationContext;
 //import antlr.ExprParser.ParenthesizedContext;
 //import antlr.ExprParser.VariableContext;
-//import expression.*;
+import antlr.ProjectParser.*;
+import antlr.ProjectParser.BinaryExprContext;
+import antlr.ProjectBaseVisitor;
+import expression.*;
+import expression.binary.*;
 //
-//public class AntlrToExpression extends ExprBaseVisitor<Expression> {
+public class AntlrToExpression extends ProjectBaseVisitor<Expression> {
+    private Map<String, Variable> vars; //stores all the variables declared in the program so far
+    private List<String> semanticErrors; //1. duplicate declaration 2. reference to undeclared variable
+    //Note that semantic errors are different from syntactic errors
+
+
+    public AntlrToExpression(List<String> semanticErrors, Map<String, Variable> vars) {
+        this.vars = vars;
+        this.semanticErrors = semanticErrors;
+
+    }
+
+    public Expression visitBinaryExpr(BinaryExprContext ctx){
+        Expression left = visit(ctx.getChild(0));// recursively visit the left subtree of the current addition node
+        Expression right = visit(ctx.getChild(2));
+        if(ctx.getChild(1).getText().equals("+")) {
+        	return new BiAddition(left, right);
+        }
+        else if(ctx.getChild(1).getText().equals("-")) {
+        	return new BiSubtraction(left, right);
+        }
+        else if(ctx.getChild(1).getText().equals("/")) {
+        	return new BiDivision(left, right);
+        }
+        else if(ctx.getChild(1).getText().equals("*")) {
+        	return new BiMultiplication(left, right);
+        }
+        else if(ctx.getChild(1).getText().equals("&&")) {
+        	return new BiConjunction(left, right);
+        }
+        else if(ctx.getChild(1).getText().equals("||")) {
+        	return new BiDisjunction(left, right);
+        }
+        else if(ctx.getChild(1).getText().equals(">")) {
+        	return new BiLarger(left, right);
+        }
+        else if(ctx.getChild(1).getText().equals("<")) {
+        	return new BiSmaller(left, right);
+        }
+        else if(ctx.getChild(1).getText().equals(">=")) {
+        	return new BiLargerOrEqual(left, right);
+        }
+        else if(ctx.getChild(1).getText().equals("<=")) {
+        	return new BiSmallerOrEqual(left, right);
+        }
+        else if(ctx.getChild(1).getText().equals("==")) {
+        	return new BiEquivalence(left, right);
+        }
+        else {
+        	return new BiNotEqual(left, right);
+        }
+
+    }
+    
+    public Expression visitUnaryExpr(UnaryExprContext ctx) {
+    	Expression expr = visit(ctx.getChild(1));
+    	if(ctx.getChild(0).getText().equals("!")) {
+    		return new Negation(expr);
+    	}
+    	/*will add more cases as more features are implemented*/
+    	return new Negation(expr);
+    	
+    }
+    
+    public Expression visitVarExpr(VarExprContext ctx) {
+    	//Expression expr = visit(ctx.getChild(0));
+		Token idToken = ctx.ID().getSymbol();
+		int line = idToken.getLine();
+		int column = idToken.getCharPositionInLine() + 1;
+		String id = ctx.getChild(0).getText();
+		
+    	if(!vars.containsKey(ctx.getChild(0).getText())) {
+    		semanticErrors.add("Error: variable " + id + " not declared (" + line + ", " + column + ")");
+    	}
+    	return vars.get(ctx.getChild(0).getText());
+    }
+    
+    public Expression visitParenthesizedExpression(ParenthesizedExprContext ctx) {
+    	Expression expr = visit(ctx.getChild(1));
+    	expr.parenthesized = true;
+    	return expr;
+    }
+    
+    public Expression visitBoolNumExpression(BoolNumExprContext ctx) {
+    	String exp = ctx.getChild(0).getText();
+		Token idToken = ctx.getToken(0,1).getSymbol();
+		int line = idToken.getLine();
+		int column = idToken.getCharPositionInLine() + 1;
+    	
+    	if(exp.charAt(0) == ('\"')) {
+    		return new StringConst(exp.substring(1,exp.length()-1));
+    	}else if(exp.equals("true")) {
+    		return new BoolConst(true);
+    	}else if(exp.equals("false")) {
+    		return new BoolConst(false);
+    	}else if(exp.charAt(0) >= '0' || exp.charAt(0) <= '9') {
+    		return new NumConst(Integer.parseInt(exp));
+    	}else {
+    		semanticErrors.add("Error: expression: " + exp + " is not a valid constant (" + line + ", " + column + ")");
+    		return new BoolConst(false);
+    	}
+    }
 //	/*
 //	 * 
 //	 */
@@ -180,4 +287,4 @@
 ////		// TODO Auto-generated method stub
 ////		return super.visitFalse(ctx);
 ////	}
-//}
+}
